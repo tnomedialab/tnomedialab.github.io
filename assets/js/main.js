8a7bef2ea74ec417c83eaefb0ce94bce12f90047
Galit <galit.rahim@tno.nl>
2019-04-24 16:38:06 +0200

    Testing new stuff for new version

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,15 @@
 	});
 
 })(jQuery);
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function smallScreens() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
